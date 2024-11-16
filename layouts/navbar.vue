5573923a0446dc9bda73669565e204193afb88ab
ZhangZJ0906 <cklgy88@gmail.com>
2024-11-17 00:57:50 +0800

    Day 6 新增navbar

---
+++ layouts/navbar.vue
@@ -0,0 +1,40 @@
+<script setup></script>
+
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon ">sss</span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <NuxtLink class="nav-link active" to="/">首頁</NuxtLink>
+                    </li>
+                    <li class="nav-item">
+                        <NuxtLink class="nav-link active"  to="/DayTwo">DayTwo</NuxtLink>
+                    </li>
+                    <li class="nav-item">
+                        <NuxtLink class="nav-link active"  to="/D3">DayThree</NuxtLink>
+                    </li>
+                    <li class="nav-item">
+                        <NuxtLink class="nav-link active"  to="/DayFour">DayFour</NuxtLink>
+                    </li>
+                    <li class="nav-item">
+                        <NuxtLink class="nav-link active"  to="/DayFive">DayFive</NuxtLink>
+                    </li>
+                    <li class="nav-item">
+                        <NuxtLink  class="nav-link active"  to="/D6/">DaySix</NuxtLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main class="container">
+        <slot />
+    </main>
+</template>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,19 +4,13 @@ const route=useRoute();
 </script>
 
 <template>
-  <NuxtLink to="/DayTwo">DayTwo</NuxtLink>
-  <br>
-  <NuxtLink to="/D3">DayThree</NuxtLink>
-  <br>
-  <NuxtLink to="/DayFour">DayFour</NuxtLink>
-  <br>
-  <NuxtLink to="/DayFive">DayFive</NuxtLink>
-  <br>
-  <NuxtLink to="/D6/">DaySix</NuxtLink>
+  
+<NuxtLayout name = "navbar">
   <div>
     Page: index
     <h1>目前路由的路徑 - route.fullPath 為: {{ route.fullPath }}</h1>
   </div>
+  </NuxtLayout>
 </template>
 
 <style scoped></style>
