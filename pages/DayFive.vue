3b74e0419adc721bffa91dce5aa4a8c419f4068a
ZhangZJ0906 <cklgy88@gmail.com>
2024-11-16 23:22:27 +0800

    DAY 5 新增 composables

---
+++ pages/DayFive.vue
@@ -0,0 +1,43 @@
+<script setup>
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/css/index.css";
+import { ref } from "vue";
+const api_url = "https://nuxr3.zeabur.app/"
+const newsList = ref([]);
+const isLoading = ref(true);
+
+// API 路徑 : https://nuxr3.zeabur.app/api/v1/home/news/
+// 使用 ES6 fetch() 或是 axios.get() 串接 API
+// 切換 isLoading 狀態
+
+fetch(api_url + "api/v1/home/news/")
+    .then((res) => res.json())
+    .then((data) => {
+        newsList.value = data.result;
+        isLoading.value = false;
+        console.log(data);
+    })
+    .catch((error) => {
+        console.error("API 调用失败：", error);
+        isLoading.value = false;
+    });
+
+
+</script>
+
+<template>
+    <div class="container">
+        <h1>最新消息</h1>
+        <NewsCard v-for="item in newsList" :key="item._id" v-bind="item" />
+        <ClientOnly>
+            <Loading v-model:active="isLoading" />
+        </ClientOnly>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    max-width: 1000px;
+    margin: 0 auto;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,8 @@ const route=useRoute();
   <NuxtLink to="/D3">DayThree</NuxtLink>
   <br>
   <NuxtLink to="/DayFour">DayFour</NuxtLink>
+  <br>
+  <NuxtLink to="/DayFive">DayFive</NuxtLink>
   <div>
     Page: index
     <h1>目前路由的路徑 - route.fullPath 為: {{ route.fullPath }}</h1>
